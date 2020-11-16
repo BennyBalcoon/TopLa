a1938e27cd2f8e4b859691debea90b944a0b4484
Benoit Balcon <benoit.b.balcon@renault.com>
2020-11-16 17:44:20 +0100

    display page for not authenticated users

---
+++ src/pages/NotAuthenticatedPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <ErrorView
+      v-bind:title="'Ooooops, vous devez vous authentifier pour visiter cette page, merci de vous connecter.'"
+      v-bind:status="'401'"
+      v-bind:navigateToPage="'LoginPage'"
+      v-bind:navigateToText="'Retourner à la page de connection'"
+    />
+  </div>
+</template>
+
+<script>
+import ErrorView from "@/components/ErrorView";
+export default {
+  components: {
+    ErrorView,
+  },
+};
+</script>
+
+<style scoped></style>
